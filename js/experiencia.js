8d9b97e668a5671b0500400341be98df5460cd63
MVReyna <vickyreyna79@gmail.com>
2023-01-24 13:10:07 -0300

    agregando ventama modal a la sección experiencia

--- js/experiencia.js
+++ js/experiencia.js
@@ -90,6 +90,8 @@ for (let i = 0; i < expLab3.length; i++) {
 
 //Info extra // 
 
+//Info Primer modal//
+
 const masInfo1 = [{
         nombre: 'Profesora de nivel Secundario de  Ciencias Naturales y afines',
         descripcion: '(Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo exercitationem voluptatem placeat veritatis, saepe laudantium ipsum accusantium beatae nesciunt vel cum quas, doloribus dolores illo facilis ipsa nulla? Fugiat, dolorem?)',
@@ -108,72 +110,99 @@ const masInfo1 = [{
 ]
 
 
-const open = document.getElementById('open');
-const modal_container1 = document.getElementById('modal_container1');
-const close = document.getElementById('close');
+let modal_body1 = document.getElementById('masInfo1');
 
-open.addEventListener('click', () => {
-    modal_container1.classList.add('show');
-    alert("abre");
-});
-
-close.addEventListener('click', () => {
-    modal_container1.classList.remove('show');
-    alert("cierra");
-});
-
-
-
-/**
 for (let i = 0; i < masInfo1.length; i++) {
-    let name = `<tr><h4>${masInfo1[i].nombre}</h4></tr>`;
-    let description = `<tr><p> ${masInfo1[i].descripcion}</p></tr>`;
-    let year = `<tr><h6>${masInfo1[i].año}</h6></tr>`;
+    let name = `<td>${masInfo1[i].nombre}</td>`;
+    let description = `<td>${masInfo1[i].descripcion}</td>`;
+    let year = `<td>${masInfo1[i].año}</td>`;
 
+    modal_body1.innerHTML += `<tr>${name + description + year}</tr>`;
 };
-*/
 
-
-
-/**
-let btnModal = document.getElementById('tbodyMas');
-
-// Botón que abre el modal
-var boton = document.getElementById("abrirModal");
-
-// Hace referencia al elemento <span> que tiene la X que cierra la ventana
-var span = document.getElementsByClassName("cerrar")[0];
-
-// Cuando el usuario hace click en el botón, se abre la ventana
-boton.addEventListener("click", function() {
-    modal.style.display = "block";
-});
-
-// Si el usuario hace click en la x, la ventana se cierra
-span.addEventListener("click", function() {
-    modal.style.display = "none";
-});
-
-// Si el usuario hace click fuera de la ventana, se cierra.
-window.addEventListener("click", function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+//Info 2do Modal
+const masInfo2 = [{
+        nombre: 'Profesora de nivel Secundario de  Ciencias Naturales y afines',
+        descripcion: '(Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo exercitationem voluptatem placeat veritatis, saepe laudantium ipsum accusantium beatae nesciunt vel cum quas, doloribus dolores illo facilis ipsa nulla? Fugiat, dolorem?)',
+        año: '(2012 al presente)'
+    },
+    {
+        nombre: 'Asistente de Investigación',
+        descripcion: '(Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo exercitationem voluptatem placeat veritatis, saepe laudantium ipsum accusantium beatae nesciunt vel cum quas, doloribus dolores illo facilis ipsa nulla? Fugiat, dolorem?)',
+        año: '(2004 - 2010)'
+    },
+    {
+        nombre: 'Experiencia 3',
+        descripcion: '(Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo exercitationem voluptatem placeat veritatis, saepe laudantium ipsum accusantium beatae nesciunt vel cum quas, doloribus dolores illo facilis ipsa nulla? Fugiat, dolorem?)',
+        año: '(2012 al presente)'
     }
-});
+]
 
-/**
-    function masInfo() {
-        for (let i = 0; i < myModal.length; i++) {
-            let cargo = `<td>${myModal[i].nombre}</td>`;
-            let description = `<td>${myModal[i].descripcion}</td>`
-            let year = `<td>${myModal[i].año}</td>`;
-        }
+let modal_body2 = document.getElementById('masInfo2');
+
+for (let i = 0; i < masInfo2.length; i++) {
+    let name = `<td>${masInfo2[i].nombre}</td>`;
+    let description = `<td>${masInfo2[i].descripcion}</td>`;
+    let year = `<td>${masInfo2[i].año}</td>`;
+
+    modal_body2.innerHTML += `<tr>${name + description + year}</tr>`;
+};
+
+//Info 3er Modal
+const masInfo3 = [{
+        nombre: 'Profesora de nivel Secundario de  Ciencias Naturales y afines',
+        descripcion: '(Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo exercitationem voluptatem placeat veritatis, saepe laudantium ipsum accusantium beatae nesciunt vel cum quas, doloribus dolores illo facilis ipsa nulla? Fugiat, dolorem?)',
+        año: '(2012 al presente)'
+    },
+    {
+        nombre: 'Asistente de Investigación',
+        descripcion: '(Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo exercitationem voluptatem placeat veritatis, saepe laudantium ipsum accusantium beatae nesciunt vel cum quas, doloribus dolores illo facilis ipsa nulla? Fugiat, dolorem?)',
+        año: '(2004 - 2010)'
+    },
+    {
+        nombre: 'Experiencia 3',
+        descripcion: '(Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo exercitationem voluptatem placeat veritatis, saepe laudantium ipsum accusantium beatae nesciunt vel cum quas, doloribus dolores illo facilis ipsa nulla? Fugiat, dolorem?)',
+        año: '(2012 al presente)'
     }
+]
 
+let modal_body3 = document.getElementById('masInfo3');
 
+for (let i = 0; i < masInfo3.length; i++) {
+    let name = `<td>${masInfo3[i].nombre}</td>`;
+    let description = `<td>${masInfo3[i].descripcion}</td>`;
+    let year = `<td>${masInfo3[i].año}</td>`;
 
-    document.getElementById('myModal').addEventListener("click", function(masInfo) {
-        console.log("Hola Mundo desde EventListener");
-        btnMas.innerHTML += `<tr>${cargo + description + year}</tr>`;
+    modal_body3.innerHTML += `<tr>${name + description + year}</tr>`;
+};
+
+//Funcionamiento Multiple Modal//
+
+const btns = document.querySelectorAll("[data-target]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        document.querySelector(btn.dataset.target).classList.add("active");
+        overlay.classList.add("active");
+        console.log("Abre la ventana modal");
     });
-     */
+});
+
+close_modals.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const modal = btn.closest(".modal");
+        modal.classList.remove("active");
+        overlay.classList.remove("active");
+        console.log("Cierra la ventana modal");
+    });
+});
+
+window.onclick = (event) => {
+    if (event.target == overlay) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach((modal) => modal.classList.remove("active"));
+        overlay.classList.remove("active");
+    }
+};
